a67fe8aa660f9b74fa3e4d50e41c6e2e1ac8a0d4
Dirk Dittmar <dirk.dittmar@web.de>
2016-03-06 20:42:38 +0100

    first try

--- gpio.py
+++ gpio.py
@@ -3,3 +3,18 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO! This is probably because you need superuser privileges. You can achieve this by"
           " using 'sudo' to run your script")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+try:
+    print "waiting..."
+    GPIO.wait_for_edge(17, GPIO.RISING)
+    print "click detected!"
+
+except KeyboardInterrupt:
+    GPIO.cleanup
+GPIO.cleanup
+
+#x = raw_input("Hit Enter to exit");
+#print(x);
