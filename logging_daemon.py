bfdfe1f16c1ae01144b21fa68f2b9987829f5a80
Dirk Dittmar <dirk.dittmar@web.de>
2016-03-09 12:16:57 +0100

    logging deamon

--- logging_daemon.py
+++ logging_daemon.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
 import json
 import logging
 import logging.config
 
 
+class MyLogger(object):
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+
+    def write(self, message):
+        # Only log if there is a message (not just a new line)
+        if message.rstrip() != "":
+            self.logger.log(self.level, message.rstrip())
+
+
 def configure_logging():
-    """
-    configures logging for this script
-    """
     with open("logging.json") as logging_json:
         data = json.load(logging_json)
     logging.config.dictConfig(data)
@@ -21,7 +30,8 @@ configure_logging()
 
 logger.info("start")
 try:
-    raw_input()
+    while True:
+        time.sleep(60)  # sleep 60s
 except KeyboardInterrupt:
     logger.info("KeyboardInterrupt")  # on Ctrl+c
 
