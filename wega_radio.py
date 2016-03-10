9fd193cf088cef8d0816195ba5cfaa382bee0dbf
Dirk Dittmar <dirk.dittmar@web.de>
2016-03-10 21:57:09 +0100

    wega radion small refactoring

--- wega_radio.py
+++ wega_radio.py
@@ -16,7 +16,7 @@ except RuntimeError:
           " using 'sudo' to run your script")
 
 
-class MusicDaemonClient(object):
+class MusicDaemonClient:
     """
     Class to control the MusicPlayerDaemon
     """
@@ -32,11 +32,18 @@ class MusicDaemonClient(object):
         self.log.debug("mpd connected at {}:{}".format(mpd_host, mpd_port))
 
     def stop(self):
+        """
+        stop playback
+        """
         self.log.debug("stop playback and clear the queue")
         self.client.stop()  # stop playing
         self.client.clear()  # clear queue
 
     def play(self, uri):
+        """
+        start playback
+        :param uri: the uri to play
+        """
         self.stop()
         self.log.debug("play: {}".format(uri))
         self.client.add(uri)
@@ -51,7 +58,7 @@ class MusicDaemonClient(object):
         self.client.disconnect()  # disconnect from mpd
 
 
-class GpioClient(object):
+class GpioClient:
     """
     Client for the GPIO stuff
     """
@@ -79,7 +86,7 @@ class GpioClient(object):
         GPIO.cleanup()
 
 
-class WegaRadioControl(object):
+class WegaRadioControl:
     """
     Controls the WEGA radio
     """
@@ -87,39 +94,61 @@ class WegaRadioControl(object):
     SWITCH_OFF_GPIO_CHANNEL = 17
     RADIO_GPIO_CHANNELS = [5, 6, 13, 19]
 
-    def __init__(self, mpd_client, gpio_client):
+    def __init__(self):
         self.log = logging.getLogger("WegaRadioControl")
-        self.mpdClient = mpd_client
-        self.gpioClient = gpio_client
+        self.mpdClient = MusicDaemonClient(config['mpd_host'], config['mpd_port'])
+        self.gpioClient = GpioClient()
         self.stations = dict()
 
-        # register callback to switch the radio off
-        self.gpioClient.add_input_channel_callback(self.SWITCH_OFF_GPIO_CHANNEL, GPIO.FALLING, self._switch_off_callback)
+        # configure the station callbacks
+        self._setup_stations(config['stations'])
 
-    def setup_stations(self, stations):
+        # register a callback to switch the radio off
+        self.gpioClient.add_input_channel_callback(WegaRadioControl.SWITCH_OFF_GPIO_CHANNEL, GPIO.FALLING,
+                                                   self._switch_off_callback)
+
+    def _setup_stations(self, stations):
+        """
+        Setup the stations
+        :param stations: a list with stations
+        """
         self.log.debug("setup stations")
+
         if len(stations) > 4:
             self.log.warn("You defined more than 4 stations. I only setup the first 4 stations in your list.")
         if len(stations) < 4:
             self.log.warn("You defined less than 4 stations. I setup the buttons von left to right with your {}"
                           " stations".format(len(stations)))
+
         num_stations = 4 if len(stations) >= 4 else len(stations)
         for i in range(num_stations):
-            ch = self.RADIO_GPIO_CHANNELS[i]
+            ch = WegaRadioControl.RADIO_GPIO_CHANNELS[i]
             self.gpioClient.add_input_channel_callback(ch, GPIO.RISING, self._switch_to_station)
             self.stations[ch] = stations[i]
 
     def _switch_off_callback(self):
+        """
+        Callback function for the off switch
+        """
         self.log.info("switch off the radio")
         self.mpdClient.stop()
 
     def _switch_to_station(self, channel):
+        """
+        Callback to switch to the station with the given GPIO-Channel
+        :param channel: the GPIO-Channel
+        """
         station = self.stations[channel]
         self.log.info("switch to station: {}".format(station['name']))
         self.mpdClient.play(station['uri'])
 
     def teardown(self):
+        """
+        This instance is unusable after this call!
+        """
         self.log.debug("teardown")
+        self.mpdClient.teardown()
+        self.gpioClient.teardown()
 
 
 def load_config():
@@ -148,11 +177,7 @@ if __name__ == '__main__':
     log = logging.getLogger("daemon")
 
     log.info("start main loop...")
-
-    musicClient = MusicDaemonClient(config['mpd_host'], config['mpd_port'])
-    gpioClient = GpioClient()
-    radioControl = WegaRadioControl(musicClient, gpioClient)
-    radioControl.setup_stations(config['stations'])
+    radioControl = WegaRadioControl()
     try:
         while True:
             time.sleep(300)  # sleep in seconds
@@ -161,7 +186,5 @@ if __name__ == '__main__':
         log.info("KeyboardInterrupt")  # on Ctrl+C
 
     log.info("shutting down")  # normal exit
-    musicClient.teardown()
-    gpioClient.teardown()
     radioControl.teardown()
     log.info("exit main loop...")
